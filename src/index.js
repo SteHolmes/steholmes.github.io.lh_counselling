66db2724226aa7f36b620bc8087995d59b3edb7d
Sasha <sasha_richardson@hotmail.co.uk>
2020-04-12 21:13:06 +0100

    Organise stylesheets, tidy up dist folder, add svg logo and revise H1 headings

--- src/index.js
+++ src/index.js
@@ -3,11 +3,8 @@ import './scss/app.scss';
 
 import './scripts/navigation.js';
 
-import sandDune from './images/sand_dune.jfif';
-import branch from './images/branch.jfif';
-import kite from './images/kite.jfif';
+import logo from './images/lighthouse_logo.svg';
 import lighthouse from './images/lighthouse.jfif';
 import beachKite from './images/beach_kite.jfif';
-import lake from './images/lake_scene.jfif';
 import footprints from './images/footprints.jfif';
 import profile from './images/profile.jpg';
