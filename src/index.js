88bfe43c8c353e0017410ccdfdf6ba57b05cc6ee
Steve <s.holmes@ctidigital.com>
2020-08-03 00:13:09 +0100

    Content update. Add container to navbar. Various styling tweaks

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import 'lodash';
 import './scss/app.scss';
 
 import './scripts/navigation.js';
-import './scripts/scroll-reveal.js';
 
 import logo from './images/lighthouse_logo.svg';
 import lighthouse from './images/lighthouse.jfif';
 import beachKite from './images/beach_kite.jfif';
 import footprints from './images/footprints.jfif';
-import profile from './images/profile.jpg';
+import profile from './images/website-profile-square-min.jpg';
 import leaves from './images/autumn_leaves.jpg';
