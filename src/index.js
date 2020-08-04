cab2b2ebbb0cc69e763bb355cf2102b2cef764e9
Steve <s.holmes@ctidigital.com>
2020-08-05 00:23:13 +0100

    Content updates. Add scroll to top button. Reposition hero header buttons. Amend text line heights.

--- src/index.js
+++ src/index.js
@@ -9,3 +9,24 @@ import beachKite from './images/beach_kite.jfif';
 import footprints from './images/footprints.jfif';
 import profile from './images/website-profile-square-min.jpg';
 import leaves from './images/autumn_leaves.jpg';
+
+
+// 'Back to top' button functionality //
+
+const scrollButton = document.getElementById("scrollBtn");
+
+window.onscroll = function() {scrollFunction()};
+scrollButton.addEventListener('click', scrollToTop)
+
+function scrollFunction() {
+  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+    scrollButton.style.display = "block";
+  } else {
+    scrollButton.style.display = "none";
+  }
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
