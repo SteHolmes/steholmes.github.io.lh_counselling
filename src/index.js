ac5b6e99e77679b0d6b211e5cc037a2c0fa9ed40
Steve <s.holmes@ctidigital.com>
2020-10-08 20:58:51 +0100

    Small text edits. Update profile image and remove un-used files

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import logo from './images/lighthouse_logo.svg';
 import lighthouse from './images/lighthouse.jfif';
 import beachKite from './images/beach_kite.jfif';
 import footprints from './images/footprints.jfif';
-import profile from './images/website-profile-square-min.jpg';
+import profile from './images/website-profile-photo.jpg';
 import leaves from './images/autumn_leaves.jpg';
 
 
